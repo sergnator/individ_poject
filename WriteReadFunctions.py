628abcc38bac3079a9285d7e24d826d2146e219c
home1 <sergnator.de@gmail.com>
2023-11-28 19:45:27 +0300

    hotfix 1

--- WriteReadFunctions.py
+++ WriteReadFunctions.py
@@ -7,9 +7,15 @@ def check_password(username: str, password: str):
     cur = con.cursor()
     req = f'select * from users where username = "{username}"'
     response = cur.execute(req).fetchall()
+    response = list(map(str, response))
     if len(response) == 0:
         raise NotFoundUsername('Такого пользователя нет')
+
     if response[0][2] == password:
+        print(1)
         data = {'username': username, 'password': response[0][2], 'date': response[0][3]}
         return data
     raise MismatchPassword('Пароль не совпадает')
+
+
+check_password(input(), input())
