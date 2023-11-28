0b8d791fc051695d0eef536b1ff34982d724a6db
home1 <sergnator.de@gmail.com>
2023-11-28 19:39:58 +0300

    alfa 1.0

---
+++ MainClasses.py
@@ -0,0 +1,16 @@
+# Класс ошибок
+class BasePostsException(Exception):
+    def __init__(self, message):
+        super().__init__()
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+class MismatchPassword(BasePostsException):
+    pass
+
+
+class NotFoundUsername(BasePostsException):
+    pass
---
+++ WriteReadFunctions.py
@@ -0,0 +1,15 @@
+import sqlite3
+from MainClasses import *
+
+
+def check_password(username: str, password: str):
+    con = sqlite3.connect('users.sqlite')
+    cur = con.cursor()
+    req = f'select * from users where username = "{username}"'
+    response = cur.execute(req).fetchall()
+    if len(response) == 0:
+        raise NotFoundUsername('Такого пользователя нет')
+    if response[0][2] == password:
+        data = {'username': username, 'password': response[0][2], 'date': response[0][3]}
+        return data
+    raise MismatchPassword('Пароль не совпадает')
