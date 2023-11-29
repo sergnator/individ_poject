748d5998bcd7a43a6307a86943162d31e18d5085
sergnator <86975413+sergnator@users.noreply.github.com>
2023-11-29 21:04:05 +0300

    alfa 2.1

--- server.py
+++ server.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/view/<filename>')
 def html_render(filename):
     return render_template(f'{filename}.html')
+
+
+@app.route('/')
+def main():
+    return html_render('index')
+
+
+if __name__ == '__main__':
+    app.run()
